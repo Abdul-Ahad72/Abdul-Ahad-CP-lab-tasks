9af1bf04f03dd747c06124caac9166611e3c6c55
Abdul Ahad <abdul15945778@gmail.com>
2026-03-02 21:45:26 +0500

    Refactor Escape.cpp with author information

--- CP LAB 1/4_Escape.cpp
+++ CP LAB 1/4_Escape.cpp
@@ -1,5 +1,4 @@
-     // Fig. 1.2: fig01_02.cpp
-    // A first program in C++.
+    // name : abdulahad ; etea :5124772
 #include <iostream>// include library for input and output
 using namespace std; // allow standard names like "cout and cin" without std::
 // function main begins program execution
@@ -10,4 +9,5 @@ int main() // it is sttarting point of program
     cout << "\t I am programming enthusiast!";// \t is used to give 1 tab space which is typically 8 spaces
     return 0;   // indicate that program ended successfully
     } // end function main
+
 
