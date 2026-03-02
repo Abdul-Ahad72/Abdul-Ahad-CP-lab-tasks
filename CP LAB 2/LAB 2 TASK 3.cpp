2dc985b056c003b3a334b9a88def79b3cb5bd2eb
Abdul Ahad <abdul15945778@gmail.com>
2026-03-02 23:19:05 +0500

    Update LAB 2 TASK 3.cpp

--- CP LAB 2/LAB 2 TASK 3.cpp
+++ CP LAB 2/LAB 2 TASK 3.cpp
@@ -1,15 +1,16 @@
-
+// name : abdulahad ; etea :5124772
 #include <iostream>
 using namespace std;
 int main()
     {
-    	double F; // WORDS LIKHNY K LIYE
+    	double F; // bari decimal value LIKHNY K LIYE
     	float C; // DECIMAL VALUE K LIYE 
-    	cout<<"enter temperature in celcius ";  // DISPLAY
-    	cin>>C;
-    F = (C * 9/5) + 32;  // FORMULA CONVERRSION K LIYE
-    	cout<<F <<"  farheight";
+    	cout<<"enter temperature in celcius ";  // DISPLAY user ko kara ga 
+    	cin>>C; //user sa value in kara ga  
+    F = (C * 9/5) + 32;  // FORMULA CONVERRSION K LIYE  
+    	cout<<F <<"  farheight";// user ko value ko show kara ga 
 	
 
 return 0;
 }
+
