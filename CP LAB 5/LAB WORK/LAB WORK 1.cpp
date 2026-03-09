3907e5c1ac7c0efbadc4d60423784b7989c90c27
Abdul Ahad <abdul15945778@gmail.com>
2026-03-09 20:42:09 +0500

    Update LAB WORK 1.cpp

--- CP LAB 5/LAB WORK/LAB WORK 1.cpp
+++ CP LAB 5/LAB WORK/LAB WORK 1.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
+// Name: Abdul Ahad
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0692
+// Section: B
+
+
+#include <iostream> //header file
 using namespace std;
 
-int main()
+int main() // start of function
 {
-    int k = 1;
+    int k = 1; // k ko declare kiya 
 
-    while (k <= 5)
+    while (k <= 5) // while ma condition lagai ha
     {
-        cout << "Print this line\n";
-        k++;
+        cout << "Print this line\n"; //line show ho gi 
+        k++; // incriment operator
     }
 
     return 0;
 }
+
