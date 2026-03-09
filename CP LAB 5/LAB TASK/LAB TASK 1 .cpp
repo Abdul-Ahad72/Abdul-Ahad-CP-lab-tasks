63b5b77881b56b79c7aafd4bbec5765305381174
Abdul Ahad <abdul15945778@gmail.com>
2026-03-09 20:13:56 +0500

    Update LAB TASK 1 .cpp

--- CP LAB 5/LAB TASK/LAB TASK 1 .cpp
+++ CP LAB 5/LAB TASK/LAB TASK 1 .cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+// Name: Abdul Ahad
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0692
+// Section: B
+
+
+
+#include <iostream> //heade file ha 
 using namespace std; 
-int main()
+int main() // function idher sa start ho ga 
 {
 	int q; // q ko decelare kiya ha
 	do{
@@ -9,7 +16,8 @@ int main()
 	}
 	while(q<=10);// ya tab tag q ki value zayada ho gi jab tak q ki value 10 ka equal na ho jaya
 	{
-		return 0;
+		return 0;// idher function end ho ga or 0 return kara ga
 	}
 
 }
+
