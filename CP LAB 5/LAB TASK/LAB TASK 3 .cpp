2af9dfc5440dd18ffd61d85fbc4afecc9356baee
Abdul Ahad <abdul15945778@gmail.com>
2026-03-09 20:34:36 +0500

    Update LAB TASK 3 .cpp

--- CP LAB 5/LAB TASK/LAB TASK 3 .cpp
+++ CP LAB 5/LAB TASK/LAB TASK 3 .cpp
@@ -6,9 +6,9 @@
 
 
 
-#include <iostream>
+#include <iostream> // header file 
 using namespace std;
-int main()
+int main() //function start idher sa ho ga
 {
 	int e; // int e decleare kiys ha user sa value lana ka liya
 	cout<<"enter positive integer : ";
@@ -19,9 +19,10 @@ int q= 1;
 while(q<=e) //while loop ha  /*jab tak q ki value e ka equal nhi ho giya loop chalta raha ga       */
 
 {
-sum += q;
-q++;	
+sum += q; // sum karna ka liya ha sum+= q is ka matlib ha sum =sum +q
+q++;	 // q ki value incriment
 }
 cout<<"sum from 1 to :"	<< e <<" is "<<sum; // output show kara ga
 
 }
+
